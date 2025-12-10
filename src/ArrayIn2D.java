41798613ab729926ce9ddcfb69955fdf35d92ad1
S Karthik <skarthik.link@gmail.com>
2025-12-10 16:54:09 +0530

    Functional program added

---
+++ src/ArrayIn2D.java
@@ -0,0 +1,3 @@
+public interface ArrayIn2D {
+
+}
--- src/QuotientRemainder.java
+++ src/QuotientRemainder.java
@@ -2,7 +2,7 @@
 
 import java.util.*;
 
-public class ComputeQuotientRemainder {
+public class QuotientRemainder {
     public static void main(String[] args){
         System.out.print("Enter two numbers : ");
         Scanner sc = new Scanner(System.in);
